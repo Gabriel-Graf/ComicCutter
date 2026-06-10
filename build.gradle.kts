1a092919c7cf1f478f25aa9d0d9fc857c724ecad
gabriel <ggraf@tegtmeier-inkubator.de>
2026-06-10 14:08:54 +0200

    build(detector): maven-publish nach mavenLocal (com.panela:comic-cutter:0.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
-plugins { kotlin("jvm") }
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+}
+group = "com.panela"
+version = "0.1.0"
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 kotlin { jvmToolchain(21) }
 tasks.test { useJUnitPlatform() }
+publishing {
+    publications { create<MavenPublication>("maven") { artifactId = "comic-cutter"; from(components["java"]) } }
+}
